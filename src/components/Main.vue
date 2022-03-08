22786a6839bd4b666146833eb7f30e0f0d001e29
alemica97 <alessandro.micalizzi08@gmail.com>
2022-03-08 18:13:20 +0100

    componente del main

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        main
+    <div class="main-wrapper">
+        <div class="container">
+          <h4>{{ mainTitle }}</h4>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   name: 'myMain',
+  data(){
+    return{
+      mainTitle: '--> Content goes here <--'
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
+
+  .main-wrapper{
+    padding: 60px 0;
+    font-size: 20px;
+    color: white;
+    background-color: #464646;
+  }
 
 </style>
